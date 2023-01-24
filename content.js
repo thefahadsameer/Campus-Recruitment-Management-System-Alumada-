d3431a56aa0d77879841a56bff311ef2093533ad
thefahadsameer <90775599+thefahadsameer@users.noreply.github.com>
2023-01-24 22:20:22 +0530

    Add files via upload

---
+++ content.js
@@ -0,0 +1,39 @@
+const button1 = document.querySelector('.button');
+
+button1.addEventListener('click', function() {
+  window.location.href = 'studentinfopage1.html'
+});
+
+
+
+
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
